f8fd6a8b2d67038616ba91e435ea26221908ed60
zenkkor <zenkkor@gmail.com>
2016-11-06 12:37:44 +0100

    Add base mocha test for simple wallet

---
+++ test/simplewallet.js
@@ -0,0 +1,16 @@
+/**
+ * Test for the simple wallet
+ */
+
+contract('SimpleWallet', function(accounts){
+
+  it('the owner is allowed to send funds', function(){
+    var myContract = SimpleWallet.deployed();
+
+    return myContract.isAllowedToSend.call(accounts[0]);.then(function(isAllowed){
+      assert(isAllowed, true, 'the owner should have been allowed to send funds');
+    });
+
+  });
+
+});
