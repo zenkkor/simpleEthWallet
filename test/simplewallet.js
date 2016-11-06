753fdd71c03be87b51a3af9772c67b9e5c1f936b
zenkkor <zenkkor@gmail.com>
2016-11-06 15:57:01 +0100

    Add test for sending wallet

--- test/simplewallet.js
+++ test/simplewallet.js
@@ -8,9 +8,19 @@ contract('SimpleWallet', function(accounts){
     var myContract = SimpleWallet.deployed();
 
     return myContract.isAllowedToSend.call(accounts[0]).then(function(isAllowed){
-      assert(isAllowed, true, 'the owner should have been allowed to send funds');
+      assert.equal(isAllowed, true, 'the owner should have been allowed to send funds');
     });
 
+  });
+
+
+  it('the other account should not be allowed to send funds', function(){
+
+    var myContract = SimpleWallet.deployed();
+
+    return myContract.isAllowedToSend.call(accounts[2]).then(function(isAllowed){
+      assert.equal(isAllowed, false, 'The other account is allowed to send');
+    });
   });
 
 });
