ca3e68f6ca8155b1dddf948ae27b739939964bd9
zenkkor <zenkkor@gmail.com>
2016-11-06 18:36:07 +0100

    Show balance on the main dash page

--- app/apps/dashboard/controllers/dashboard.js
+++ app/apps/dashboard/controllers/dashboard.js
@@ -3,6 +3,9 @@ var app = angular.module('simpleEthWallet');
 
 app.controller("DashboardCtrl", function($scope) {
 
-    $scope.myVar = "Main";
+	var contract = SimpleWallet.deployed();
+
+    $scope.balance = web3.eth.getBalance(contract.address).toNumber();
+	$scope.balanceInEther = web3.fromWei($scope.balance, "ether");
 
 });
