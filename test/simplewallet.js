4f4a969fbb4fe3f3382a4dfb548ec287b68232df
zenkkor <zenkkor@gmail.com>
2016-11-06 15:51:40 +0100

    Fix test callback

--- test/simplewallet.js
+++ test/simplewallet.js
@@ -7,7 +7,7 @@ contract('SimpleWallet', function(accounts){
   it('the owner is allowed to send funds', function(){
     var myContract = SimpleWallet.deployed();
 
-    return myContract.isAllowedToSend.call(accounts[0]);.then(function(isAllowed){
+    return myContract.isAllowedToSend.call(accounts[0]).then(function(isAllowed){
       assert(isAllowed, true, 'the owner should have been allowed to send funds');
     });
 
