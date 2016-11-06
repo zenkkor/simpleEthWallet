28371b26a6433bac95a5f92891884cf476333e33
zenkkor <zenkkor@gmail.com>
2016-11-06 18:52:14 +0100

    Add withdraw methods to wallet

--- app/apps/funds/controllers/funds.js
+++ app/apps/funds/controllers/funds.js
@@ -3,33 +3,51 @@ var app = angular.module('simpleEthWallet');
 
 app.controller("FundsCtrl", function($scope) {
 
-    $scope.accounts = web3.eth.accounts;
+	$scope.accounts = web3.eth.accounts;
 
 	/**
 	 * Deposit funds
-	 * @return
+	 * @param address
+	 * @param amount
 	 */
 	$scope.depositFunds = function(address, amount) {
 		var contract = SimpleWallet.deployed();
 
-		console.log()
-
 		// Deposit Ether
-        web3.eth.sendTransaction({
-            from: address.trim(),
-            to: contract.address,
-            value: web3.toWei(amount, "ether")
-        },
-		function(error, result){
-			if ( error )
-			{
-				$scope.has_errors = "Whoops, deposit failed.";
-			}
-			else
-			{
-				$scope.transfer_success = true;
-			}
-			$scope.$apply();
-		});
+		web3.eth.sendTransaction({
+				from: address.trim(),
+				to: contract.address,
+				value: web3.toWei(amount, "ether")
+			},
+			function(error, result) {
+				if (error) {
+					$scope.has_errors = "Whoops, deposit failed.";
+				} else {
+					$scope.transfer_success = true;
+				}
+				$scope.$apply();
+			});
+	}
+
+	/**
+	 * Withdraw funds
+	 * @param address
+	 * @param amount
+	 */
+	$scope.withdrawFunds = function(address, amount) {
+		var contract = SimpleWallet.deployed();
+
+		var send_json = {
+			from: web3.eth.accounts[0]
+		};
+
+		contract.sendFunds(amount, address, send_json).then(function(newBalance) {
+				$scope.transfer_success_withdraw = true;
+				$scope.$apply();
+			},
+			function(errors) {
+				$scope.has_errors_withdraw = errors
+				$scope.$apply();
+			});
 	}
 });
